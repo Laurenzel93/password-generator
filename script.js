234a90b2c2496297e1d8b2052fe6e3b7a1952685
Lauren Wenzel <77553907+Laurenzel93@users.noreply.github.com>
2021-02-13 15:05:32 -0500

    finished code
    fixed bug that didn't clear generated password after multiple attempts. fixed issue where commas were being placed within the randomly generated password.

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  console.log(passwordText.value);
   passwordText.value = password;
-
+  console.log(passwordText.value);
 }
-
-
+// assigning variables and inputting options into arrays
 let specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "~", "?", "<", ">", "/"];
 let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -21,49 +21,36 @@ let randomPassword = "";
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
+  randomPassword = ""; // This ensures that the generated password is cleared on every new button click
 
   let passwordLength = prompt("Please enter the number of characters you want for you new password. It may be between 8 and 128 characters.");
   if (!passwordLength || passwordLength <= 7 || passwordLength >= 129) {
-    return alert("Invalid Response. Please try again.");
+    return alert("Invalid Response. Please try again."); // ensures if no response or incorrect responses are not accepted
   }
   let useLowerCase = confirm("Do you want your new password to include lowercase letters?");
   if (useLowerCase === true) {
-    masterArray += lowerCase;
-    console.log(masterArray);
+    masterArray = masterArray.concat(lowerCase);
   }
 
   let useUpperCase = confirm("Do you want your new password to include uppercase letters?");
   if (useUpperCase === true) {
-    masterArray += upperCase;
-    console.log(masterArray);
+    masterArray = masterArray.concat(upperCase);
   }
 
   let useNumbers = confirm("Do you want your new password to include numbers?");
   if (useNumbers === true) {
-    masterArray += numbers;
-    console.log(masterArray);
+    masterArray = masterArray.concat(numbers);
   }
 
   let useSpecialCharacters = confirm("Do you want your new password to include special characters?");
   if (useSpecialCharacters === true) {
-    masterArray += specialCharacters;
-    console.log(masterArray);
-  }
-  let randomPassword = " ";
-
-  //for (let i = 0; i < passwordLength; i++); {
-    //randomPassword += Math.floor(Math.random() * (masterArray.length - 0) + 0);
-
-    for (let i = 0; i < passwordLength; i++) {
-      randomPassword += masterArray[Math.floor(Math.random() * (masterArray.length))];
-      console.log(Math.floor(Math.random() * (masterArray.length)));
-      console.log(randomPassword);        
-      }
-   return randomPassword;
+    masterArray = masterArray.concat(specialCharacters);
   }
 
-  // make master array
-  // get random thing from master array
-  // put random thing into password variable
+  for (let i = 0; i < passwordLength; i++) {
+    randomPassword += masterArray[Math.floor(Math.random() * (masterArray.length))];
+  }
 
-  //return masterArray;
+  return randomPassword;
+
+}
