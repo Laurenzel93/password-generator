a4e31b43319923fb48d8d8f512b233d942824931
Lauren Wenzel <77553907+Laurenzel93@users.noreply.github.com>
2021-02-12 19:09:17 -0500

    added master array variable

--- script.js
+++ script.js
@@ -1,48 +1,53 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
- 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
- 
+
 }
- 
- 
+
+
 let specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "~", "?", "<", ">", "/"];
-let lowerCase = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let upperCase = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-let numbers = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
- 
+let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+let masterArray = [];
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
- 
- 
- 
-let passwordLength = prompt("Please enter the number of characters you want for you new password. It may be between 8 and 128 characters.");
-    if (!passwordLength || passwordLength <= 7 || passwordLength >= 129) {
-      return alert("Invalid Response. Please try again.");
-    } else {}
+
+  let passwordLength = prompt("Please enter the number of characters you want for you new password. It may be between 8 and 128 characters.");
+  if (!passwordLength || passwordLength <= 7 || passwordLength >= 129) {
+    return alert("Invalid Response. Please try again.");
+  }
   let lowerCase = confirm("Do you want your new password to include lowercase letters?");
-    if (lowerCase === true) {
-      const randomLowerCase = Math.floor(Math.random() * lowerCase.length);
-    } else {}
- 
+  if (lowerCase === true) {
+    masterArray = masterArray.concat(lowerCase);
+  }
+
   let upperCase = confirm("Do you want your new password to include uppercase letters?");
   if (upperCase === true) {
-    const randomUpperCase = Math.floor(Math.random() * upperCase.length);
-  } else {}
- 
+    masterArray = masterArray.concat(upperCase);
+  }
+
   let numbers = confirm("Do you want your new password to include numbers?");
   if (numbers === true) {
-    const randomNumbers = Math.floor(Math.random() * numbers.length);
+    masterArray = masterArray.concat(numbers);
   }
- 
+
   let specialCharacters = confirm("Do you want your new password to include special characters?");
   if (specialCharacters === true) {
-    const randomSpecialCharacters = Math.floor(Math.random() * specialCharacters.length);}
-  
+    masterArray = masterArray.concat(specialCharacters);
   }
+
+// make master array
+// get random thing from master array
+// put random thing onto password variable
+
+return masterArray;
+}
 let randomPassword = " ";
