21bf9ef6caa6eb444b2f9f87a36c5a0de3269dca
Lauren Wenzel <Laurenzel93@aol.com>
2021-10-31 21:08:24 -0400

    added const for unchanging variables

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ function writePassword() {
   console.log(passwordText.value);
 }
 // assigning variables and inputting options into arrays
-let specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "~", "?", "<", ">", "/"];
-let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "~", "?", "<", ">", "/"];
+const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 let masterArray = [];
 let randomPassword = "";
 
